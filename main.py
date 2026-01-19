98e9d49419ff22c22d51737d7affe7db62865bf0
monbed <67903399+monbed@users.noreply.github.com>
2026-01-19 15:08:56 +0800

    v1.6.7

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ load_ntr_statuses()
     "astrbot_plugin_animewifex",
     "monbed",
     "群二次元老婆插件修改版",
-    "1.6.6",
+    "1.6.7",
     "https://github.com/monbed/astrbot_plugin_animewifex",
 )
 class WifePlugin(Star):
